f6b038ffa5decafc27414f8cd17d113ec7f05cb7
Daniel Vidali Fryer <daniel-fryer@live.com.au>
2020-12-30 22:42:51 +1100

    Update Snippets.R

--- Snippets.R
+++ Snippets.R
@@ -10,6 +10,30 @@ x <- z <- 2
 ## %% Assign in opposite directions at once
 x <- 3 -> z
 
+## %% Use map to apply a function to each element
+library(purrr)
+c(1,2,3) %>% map(function(x){x^2})
+c(1,2,3) %>% map_dbl(function(x){x^2})
+c(a = 1, b = 2, c = 3) %>% map_dfr(function(x){x^2})
+
+## %% Capture string output from print
+a <- capture.output(cat("hi"))
+s <- summary(lm(mpg ~ cyl, data = mtcars))
+os <- capture.output(s)
+
+## %% Unlist a list of factors combines them well
+fac1 <- factor(c("a","a","b"))
+fac2 <- factor(c("b","b","a"))
+unlist(list(fac1, fac2))
+
+## %% Evaluate an R expression in an environment constructed from data
+df <- data.frame(x = 1:10, y = 1:10, z = rep("a",10))
+with(df, {
+  m <- x + y
+  print(paste0(z[1], " number is ", m))
+})
+
+
 
 
 
